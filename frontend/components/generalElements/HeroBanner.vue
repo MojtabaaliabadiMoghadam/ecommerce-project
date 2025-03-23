ef3f9462516ac44eaefba89674fb73a9a84eb9f1
MojtabaaliabadiMoghadam <mojiland2002@gmail.com>
2025-03-23 16:23:11 +0330

    hero section

--- frontend/components/generalElements/HeroBanner.vue
+++ frontend/components/generalElements/HeroBanner.vue
@@ -1,117 +1,108 @@
 <template>
-  <!-- <div class="relative mx-auto">
-      <img
-      width="1400"
-      height="800"
-      class="object-cover w-full h-[420px] lg:h-[560px] xl:h-[640px]"
-      src="/images/hero-4.jpg"
-      alt="Hero image"
-      loading="eager"
-      sizes="sm:100vw md:1400px"
-      preload
-      placeholder
-      placeholder-class="blur-xl" />
-    <div class="container absolute inset-0 flex flex-col items-end justify-center bg-gradient-to-l from-gray-200 md:bg-none">
-      <h1 class="text-3xl font-bold md:mb-4 md:text-4xl lg:text-6xl">Just landed.</h1>
-      <h2 class="text-lg font-bold md:mb-4 lg:text-3xl">The New Year Collection</h2>
-      <div class="max-w-sm mb-8 text-md font-light lg:max-w-md text-balance">
-        <p>Our latest collection is here. Discover the latest trends and styles for the new year.</p>
-      </div>
-      <NuxtLink class="px-6 py-3 font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-800" to="/products">Shop now</NuxtLink>
-    </div>
-  </div> -->
-  <div>
-    <section class="hero">
-      <div class="content">
-        <h2>Hero Header</h2>
-        <p>Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend
-          justo vel bibendum sapien massa ac turpis faucibus orci luctus non</p>
-      </div>
-      <div class="waves"></div>
-    </section>
+  <div class="w-full h-[50svh] px-3 pt-2">
+    <swiper :spaceBetween="30" :effect="'fade'" :navigation="true" :pagination="{ clickable: true }" :loop="true"
+      :modules="modules" class="mySwiper" :autoplay="{ delay: 7000, disableOnInteraction: false }">
+      <swiper-slide>
+        <div class="bg-[url('/images/hero-bg.svg')] overflow-hidden relative bg-no-repeat bg-cover bg-center h-full w-full flex items-center justify-center
+           md:justify-between">
+          <div class="w-1/2 hidden md:flex flex-col items-start justify-center ps-32 gap-12 h-full">
+            <div class="text-white font-medium text-[24px]">جدیدترین موبایل‌ها و لوازم جانبی با بهترین قیمت</div>
+            <div class="text-white font-bold text-[30px]">همین حالا خرید کنید و تخفیف ویژه بگیرید!</div>
+            <div class="text-white/70 font-medium text-[16px]">تنوع بی‌نظیر گوشی‌های هوشمند</div>
+          </div>
+          <div class="w-1/2 flex items-center justify-center">
+            <img class="z-20 !w-[270px] h-[270px]" src="/images/watch-apple.png" alt="">
+          </div>
+          <div class="border-2 border-[#53619b] rounded-full p-5 absolute -top-[90%] left-[12%] hidden md:block">
+            <div class="bg-[#2C3454] rounded-full h-[500px] w-[500px]">
+            </div>
+          </div>
+          <div class="border-2 border-[#53619b] rounded-full p-5 absolute top-[70%] left-[16%] hidden md:block">
+            <div class="bg-[#2C3454] rounded-full h-[150px] w-[150px]">
+            </div>
+          </div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="bg-[#FFF3CC] overflow-hidden relative bg-no-repeat bg-cover bg-center h-full w-full flex items-center justify-center
+           md:justify-between">
+          <div class="w-1/2 hidden md:flex flex-col items-start justify-center ps-32 gap-12 h-full">
+            <div class="text-black font-medium text-[24px]">جدیدترین موبایل‌ها و لوازم جانبی با بهترین قیمت</div>
+            <div class="text-black font-bold text-[30px]">همین حالا خرید کنید و تخفیف ویژه بگیرید!</div>
+            <div class="text-black/70 font-medium text-[16px]">تنوع بی‌نظیر گوشی‌های هوشمند</div>
+          </div>
+          <div class="w-1/2 flex items-center justify-center">
+            <img class="z-20 !w-[270px] h-[270px]" src="/images/watch-apple.png" alt="">
+          </div>
+          <div class="border-2 border-[#F6DE8D] rounded-full p-5 absolute -top-[90%] left-[12%] hidden md:block">
+            <div class="bg-[#F6DE8D] rounded-full h-[500px] w-[500px]">
+            </div>
+          </div>
+          <div class="border-2 border-[#F6DE8D] rounded-full p-5 absolute top-[70%] left-[16%] hidden md:block">
+            <div class="bg-[#F6DE8D] rounded-full h-[150px] w-[150px]">
+            </div>
+          </div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="bg-[#313131] overflow-hidden relative bg-no-repeat bg-cover bg-center h-full w-full flex items-center justify-center
+           md:justify-between">
+          <div class="w-1/2 hidden md:flex flex-col items-start justify-center ps-32 gap-12 h-full">
+            <div class="text-white font-medium text-[24px]">جدیدترین موبایل‌ها و لوازم جانبی با بهترین قیمت</div>
+            <div class="text-white font-bold text-[30px]">همین حالا خرید کنید و تخفیف ویژه بگیرید!</div>
+            <div class="text-white/70 font-medium text-[16px]">تنوع بی‌نظیر گوشی‌های هوشمند</div>
+          </div>
+          <div class="w-1/2 flex items-center justify-center">
+            <img class="z-20 !w-[270px] h-[270px]" src="/images/watch-apple.png" alt="">
+          </div>
+          <div class="border-2 border-[#404040] rounded-full p-5 absolute -top-[90%] left-[12%] hidden md:block">
+            <div class="bg-[#404040] rounded-full h-[500px] w-[500px]">
+            </div>
+          </div>
+          <div class="border-2 border-[#404040] rounded-full p-5 absolute top-[70%] left-[16%] hidden md:block">
+            <div class="bg-[#404040] rounded-full h-[150px] w-[150px]">
+            </div>
+          </div>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
-
 </template>
-<script setup lang="ts">
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import { EffectFade, Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+const modules = ref([EffectFade, Navigation, Pagination, Autoplay]);
 </script>
-<style lang="scss" scoped>
-
-.content {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 0 20px;
+<style>
+.swiper {
+  border-radius: 25px;
+  width: 100%;
+  height: 100%;
 }
 
-.hero {
-  position: relative;
-  background: #333333;
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+}
+
+.swiper-button-next,
+.swiper-rtl .swiper-button-prev {
   color: white;
-  height: 70vh;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
 }
 
-.hero h2 {
-  position: relative;
-  z-index: 1;
-  font-size: 4.5rem;
-  margin: 0 0 10px;
-  line-height: 1;
-  color: rgba(255, 255, 255, 0.9);
-}
-
-.hero p {
-  position: relative;
-  z-index: 1;
-  font-size: 1.1rem;
-  color: rgba(255, 255, 255, 0.5);
-  line-height: 1.4;
-}
-
-/* ========================= */
-
-.waves {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 200px;
-  background-color: rgb(0, 187, 255);
-  box-shadow: inset 0 0 50px rgba(0, 0, 0, 0.5);
-  transition: 500ms;
-}
-
-.waves::before,
-.waves::after {
-  content: '';
-  position: absolute;
-  width: 300vw;
-  height: 300vw;
-  top: -65vw;
-  left: 50%;
-  transform: translate(-50%, -75%);
-}
-
-.waves::before {
-  border-radius: 44%;
-  background: #A1E3F9;
-  animation: waves 8s linear infinite;
-}
-
-.waves::after {
-  border-radius: 44%;
-  background: #A1E3F9;
-  animation: waves 15s linear infinite;
-}
-
-@keyframes waves {
-  0% {
-    transform: translate(-50%, -75%) rotate(0deg);
-  }
-
-  100% {
-    transform: translate(-50%, -75%) rotate(360deg);
-  }
+.swiper-pagination-bullet-active {
+  background: #ffffff;
 }
 </style>
